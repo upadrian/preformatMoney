98fcfb69b3a433d326919c30ac2fd00aae65d11a
Adrián Suárez <upadrian@gmail.com>
2013-07-02 14:20:53 -0300

    code beauty, object chain

--- jquery.preformatField.js
+++ jquery.preformatField.js
@@ -13,8 +13,8 @@
  */
 ;(function($, document, window, undefined) {
     "use strict";
-    var pluginName = 'preformat';
-    var defaults = {
+    var pluginName = 'preformat',
+		defaults = {
     	//accounting.formatMoney(number,[symbol = "$"],[precision = 2],[thousand = ","],[decimal = "."],[format = "%s%v"])
         symbol		: "$",
 		precision	: 2,
@@ -23,49 +23,50 @@
 		format		: "%s%v"
     };
     function Plugin(element, options) {
-        this.element = element;
-        this.options = $.extend({}, defaults, options);
-        this.$el      = $(element);
+        this.element	= element;
+        this.options	= $.extend({}, defaults, options);
+        this.$el		= $(element);
         this.init();
     }
     Plugin.prototype = {
         init : function() {     
         	//chequeos previos
-        	if(this.preChecksFail())
+        	if(this.preChecksFail(this))
         		return false;
             var html = '<input readonly="readonly" type="text" name="pre-'+this.$el.attr("name")+'" id="pre-'+this.$el.attr("id")+'" value="" />';
 			this.$el
-				.data("display",this.$el.css("display"))
-				.after(html)
-				.css("display","none");
+					.data("display",this.$el.css("display"))
+					.after(html)
+					.css("display","none");
 			this.$target = $("#pre-"+this.$el.attr("id"));
 			this.$target
-				.css("display",this.$el.data("display"))
-				.css("cursor","pointer")
-				.attr("class",this.$el.attr("class"));
-			this.getValue();	
-			this.setPreValue(this);
-			this.setEvents(this);
+					.css("display",this.$el.data("display"))
+					.css("cursor","pointer")
+					.attr("class",this.$el.attr("class"));
+			this.getValue(this)
+				.setPreValue()
+				.setEvents();
         },
-        preChecksFail : function(){
-        	if(!this.$el.attr("id")){
+        preChecksFail : function(plugin){
+        	var plugin = plugin || this; 
+        	if(!plugin.$el.attr("id")){
         		try{
         			console.log("Preformat plugin: missed id attribute on...");
-        			console.debug(this.$el);
+        			console.debug(plugin.$el);
 				} catch(e){}
         		return true;
        		}
-       		if(!this.$el.attr("name")){
+       		if(!plugin.$el.attr("name")){
         		try{
         			console.log("Preformat plugin: missed name attribute on...");
-        			console.debug(this.$el);
+        			console.debug(plugin.$el);
 				} catch(e){}
         		return true;
        		}
-        	if(this.$el.attr("type") != "text" && this.$el.attr("type") != "number"){
+        	if(plugin.$el.attr("type") != "text" && plugin.$el.attr("type") != "number"){
         		try{
-        			console.log("Preformat plugin: type "+this.$el.attr("type")+" not supported on...");
-        			console.debug(this.$el);
+        			console.log("Preformat plugin: type "+plugin.$el.attr("type")+" not supported on...");
+        			console.debug(plugin.$el);
 				} catch(e){}
         		return true;
        		}
@@ -75,18 +76,19 @@
         			console.log("We need accounting plugin, get it on http://josscrowcroft.github.com/accounting.js");
 				} catch(e){}
         		return true;
-       		}
-       		
-       			
+       		}	
         },
-        getValue : function(){
-			this.valorOriginal = this.$el.val();
-			if(!this.isNumber(this.valorOriginal)){
-				this.valorOriginal = 0;
-				this.$el.val(0);
+        getValue : function(plugin){
+        	var plugin = plugin || this;
+			plugin.valorOriginal = plugin.$el.val();
+			if(!plugin.isNumber(plugin.valorOriginal)){
+				plugin.valorOriginal = 0;
+				plugin.$el.val(0);
 			}
+			return plugin;
     	},
     	setEvents : function(plugin){
+    		var plugin = plugin || this
 	    	plugin.$target.focus(function(){
 	   			$(this).css("display","none");
 	   			plugin.$el.css("display",plugin.$el.data("display")).focus();
@@ -98,37 +100,43 @@
 	    		})
 	    		.change(function(){
 	    			if(isNaN($(this).val())){
-	    				$(this).val("0");
+	    				$(this).val("0").change();
 	    				return false;
     				}
-	    			plugin.getValue();	
-	    			plugin.setPreValue();
+	    			plugin
+						.getValue()
+						.setPreValue();
 	    		})
 	    		.keypress(function(ev){
 	    			//http://stackoverflow.com/questions/3764821/best-way-to-restrict-a-text-field-to-numbers-only
 	    			var iKeyCode = ev.which || ev.keyCode,
 						aSpecialKeysForFirefox = [8, 9, 13, 16, 17, 27, 35, 36, 37, 38, 39, 40, 46],
 						sKey = String.fromCharCode(iKeyCode);
-					if(!sKey.match(/[0-9]/) && $.inArray(iKeyCode, aSpecialKeysForFirefox) < 0)
-							ev.preventDefault();
+					//si no es numerico, y no es un caracter permitido, y no es el separador de decimales, entonces bloqueamos
+					if(!sKey.match(/[0-9]/) && $.inArray(iKeyCode, aSpecialKeysForFirefox) < 0 && sKey != "." && sKey != "-")
+						ev.preventDefault();
 	    		})
+    		return plugin;
 	    },
     	isNumber : function(n){
     		return !isNaN(+n) && isFinite(n);
 		},
-		setPreValue : function(){
-			this.valor = accounting.formatMoney(this.valorOriginal, this.options.symbol, this.options.precision,  this.options.thousand, this.options.decimal,this.options.format); // $4.999,99
-			this.$target.val(this.valor);
+		setPreValue : function(plugin){
+			var plugin = plugin || this
+			plugin.valor = accounting.formatMoney(plugin.valorOriginal, plugin.options.symbol, plugin.options.precision,  plugin.options.thousand, plugin.options.decimal,plugin.options.format); // $4.999,99
+			plugin.$target.val(plugin.valor);
+			return plugin;
 		},
-		destroy : function(){
-			this.$el
-				.css("display",this.$el.data("display"))
-				.unbind("blur")
-				.unbind("change")
-				.unbind("keydown")
-				.data('plugin_' + pluginName,false);;
-			this.$target.remove();
-			this.$target=null;
+		destroy : function(plugin){
+			var plugin = plugin || this
+			plugin.$el
+					.css("display",this.$el.data("display"))
+					.unbind("blur")
+					.unbind("change")
+					.unbind("keydown")
+					.data('plugin_' + pluginName,false);
+			plugin.$target.remove();
+			plugin.$target=null;
 		}
     };
 
